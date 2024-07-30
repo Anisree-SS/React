af8cdc2d10e82bba68c9208f7e54e7e33d810295
Anisree-SS <anisree.ss@techversantinfotech.com>
2024-07-30 17:45:49 +0530

    committed

---
+++ Calendar/src/App.jsx
@@ -0,0 +1,109 @@
+import { startOfMonth, getDaysInMonth, subMonths, addMonths } from "date-fns";
+import { useState, useEffect } from "react";
+import SelectTag from "./SelectedTag";
+import Weeks from "./weeks";
+import Days from "./Days";
+
+function App() {
+  const [today, setToday] = useState(new Date());
+  const [newDate, setNewDate] = useState({
+    selectMonth: today.getMonth(),
+    selectyr: today.getFullYear(),
+    selectedDate: today.getDate(),
+  });
+
+  const date = today.getDate();
+  const month = today.getMonth();
+  const year = today.getFullYear();
+  const daysinMon = getDaysInMonth(today);
+
+  useEffect(()=>{
+    setToday(
+      new Date(
+        newDate.selectyr,
+        newDate.selectMonth,
+        newDate.selectMonth == 1 && newDate.selectedDate > 29
+          ? 28
+          : newDate.selectedDate
+      )
+    );
+    console.log(today);
+  },[today])
+
+  function monthSelect(event) {
+    const { name, value } = event.target;
+    setNewDate((prevDate) => ({ ...prevDate, [name]: value }));
+    setToday(
+      new Date(
+        newDate.selectyr,
+        newDate.selectMonth,
+        newDate.selectMonth == 1 && newDate.selectedDate > 29
+          ? 28
+          : newDate.selectedDate
+      )
+    );
+  }
+
+  function setNewDates(day) {
+    console.log(day);
+    setNewDate({
+      selectMonth: day.getMonth(),
+      selectyr: day.getFullYear(),
+      selectedDate: day.getDate(),
+    });
+    setToday(
+      new Date(newDate.selectyr, newDate.selectMonth, newDate.selectedDate)
+    );
+  }
+
+  const prevDay = startOfMonth(today).getDay();
+  const [updateMonth, setupdateMonth] = useState(today);
+
+  function updatePrevMonth() {
+    setupdateMonth(subMonths(today, 1));
+    setNewDates(updateMonth);
+  }
+
+  function updatePostMonth() {
+    setupdateMonth(addMonths(today, 1));
+    setNewDates(updateMonth);
+  }
+
+  function selectDate(event) {
+    monthSelect(event);
+  }
+
+  return (
+    <>
+      <div className="grid-container">
+        <div onClick={updatePrevMonth} className="buttonStyle">
+          &lt;
+        </div>
+        <div className="item1">
+          <SelectTag
+            Selectfunction={monthSelect}
+            names="selectMonth"
+            years={month}
+            len="0"
+          />
+        </div>
+        <div className="item2">
+          <SelectTag
+            Selectfunction={monthSelect}
+            names="selectyr"
+            years={year}
+            len="100"
+          />
+        </div>
+        <div onClick={updatePostMonth} className="buttonStyle">
+          &gt;
+        </div>
+        <Weeks />
+        <Days dayLen={prevDay} />
+        <Days dayLen={daysinMon} selectedDate={selectDate} date={date} />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Calendar/src/Days.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+function Days(props) {
+  const [isColor,setBgColor]=useState({backgroundColor:'',
+    keys:''
+  });
+  function setstyles(event){
+    const names =event.target.id; 
+    console.log(names);
+
+    setBgColor({
+        backgroundColor:"aliceBlue",
+        keys:names
+    });
+
+}
+console.log(isColor.keys);
+
+  const isDay = props.dayLen > 10 ? true : false;
+  return (
+    <>
+      {Array.from({ length: props.dayLen }).map((_, index) => (
+        <div style={{backgroundColor:(isColor.keys==index)?isColor.backgroundColor:''}}
+        id={index}
+        onMouseEnter={setstyles}>
+          {isDay ? (
+            <input
+              type="button"
+              className="buttonStyle"
+              onClick={props.selectedDate}
+              value={index + 1}
+              name="selectedDate"
+              style={{
+                color: index + 1 == props.date ? "red" : "black"
+              }}
+              
+            />
+          ) : (
+            ""
+          )}
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default Days;
